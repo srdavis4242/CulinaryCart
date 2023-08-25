a22a0a950fb6a2c3c76b09dbe50fd1af5662eba8
Sam <srdavis4242@gmail.com>
2023-08-25 11:29:56 -0500

    Updated design and functionality

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 document.querySelector('button').addEventListener('click', function() {
-    alert('Button clicked!');
+    alert('Button clicked!'); // Placeholder alert for button click
+});
+
+// Functionality to highlight websites on Enter press
+document.getElementById('websites').addEventListener('keyup', function(event) {
+    if (event.key === 'Enter') {
+        let content = this.value.split('\n');
+        let formattedContent = content.map(site => '<span class="highlight-pink">' + site + '</span>').join('<br>');
+        this.innerHTML = formattedContent;
+        this.scrollTop = this.scrollHeight; // Scroll to the bottom
+    }
 });
